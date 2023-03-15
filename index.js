10e9e42a803e5d21dfac33c271b984b6bc04bb71
Jtharmon <jtharmon10@gmail.com>
2023-03-15 15:26:04 -0400

    changed origin

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(cors({
-    origin: 'http://localhost:1234',
+    origin: true,
     credentials: true
 }));
 
